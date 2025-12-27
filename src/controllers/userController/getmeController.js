27defb1c85d6a5746b72c42197ee6f8736c305bf
Kumar manak <manakkash20@gmail.com>
2025-12-27 23:51:24 +0530

    feat(controller): implemented getMecontroller for user profile

---
+++ src/controllers/userController/getmeController.js
@@ -0,0 +1,20 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+
+async function getmeController(req,res) {
+    const id=req.user.id;
+    const user = await prisma.User.findUnique({
+        where:{id:id},select: {
+            id: true,
+            email: true,
+            role: true,
+            organizationId: true,
+            createdAt: true
+          }
+    })
+    if(!user){
+        return res.status(400).json({err:"User not found"})
+    }
+    return res.status(200).json(user)
+}
+module.exports=getmeController
