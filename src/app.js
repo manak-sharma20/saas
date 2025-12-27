d7220a7f3aa7cf75fa7ec729dabf57c4f3411413
Kumar manak <manakkash20@gmail.com>
2025-12-28 00:23:51 +0530

    feat(app): wire auth, project and user routes

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+const authRoute=require("./routes/authRoutes/auth.Route")
+const projectRoute=require("./routes/projectRoutes/project.Route")
+const userRoute=require("./routes/userRoutes/user.Route")
+
 
 app.use(cors());
 app.use(express.json());
-
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "ok" });
-});
+app.use("/auth",authRoute);
+app.use("/projects",projectRoute);
+app.use("/users",userRoute)
 
 module.exports = app;
